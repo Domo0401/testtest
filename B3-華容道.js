3097d9df1848f239342f12b954553bb11301eff9
Domo0401 <146755659+Domo0401@users.noreply.github.com>
2023-10-10 14:52:53 +0800

    Update B3-華容道.js

--- B3-華容道.js
+++ B3-華容道.js
@@ -5,7 +5,7 @@ const resultDiv = document.getElementById("result");
 win = 0;
 
 // Set the seed for the random number generator
-const seed = "gggg";
+const seed = "owowowo";
 const random = new Math.seedrandom(seed);
 
 // Shuffle the pieces using the seeded random number generator
